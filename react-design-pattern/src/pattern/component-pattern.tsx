1202b9276abde4828450b0af510819aa85eacf85
rahul <rkrahul8181@gmail.com>
2024-04-10 12:31:27 +0530

    component pattern

---
+++ react-design-pattern/src/pattern/component-pattern.tsx
@@ -0,0 +1,39 @@
+
+type coursePriceType = {
+    course:string,
+    price:string
+}
+const PriceCard = ({course,price}:coursePriceType)=>{
+    return (
+        <div>
+            <div>{course}</div>
+            <div>{price}</div>
+        </div>
+    )
+};
+
+
+const NodeJsCard = ()=>{
+    return (
+        <PriceCard course="Nodejs" price="100000000" />
+    )
+}
+
+const ReactJsCard = ()=>{
+    return (
+        <PriceCard course="ReactJs" price="9093490343" />
+    )
+}
+
+
+export const CoursePriceContainer = ()=>{
+    return (
+        <div>
+            <NodeJsCard />
+        <ReactJsCard />
+        </div>
+    )
+}
+
+
+
