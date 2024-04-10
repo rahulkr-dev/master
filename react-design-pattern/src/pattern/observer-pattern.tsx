121f5f5a01601bff2681ebdee732511ad5cf2a9b
rahul <rkrahul8181@gmail.com>
2024-04-10 12:41:20 +0530

    observer pattern

---
+++ react-design-pattern/src/pattern/observer-pattern.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react"
+
+
+export const ChatConainter = ()=>{
+    const [message,setMessage] = useState<string[]>([])
+    const [newMessage,setNewMessage] = useState("")
+
+    useEffect(()=>{
+        // listen message from server for updating
+    },[]);
+
+    const sendMessage = ()=>{
+        setMessage((prev)=>{
+            return [...prev,newMessage]
+        })
+    };
+
+    return (
+        <div>
+            <div>
+                {message?.map((item)=>(
+                    <p>{item}</p>
+                ))}
+            </div>
+
+            <div>
+                <input value={newMessage} onChange={({target:{value}})=>setNewMessage(value)} type="text" name="" id="" />
+                <button onClick={sendMessage}>Send</button>
+            </div>
+        </div>
+    )
+}
